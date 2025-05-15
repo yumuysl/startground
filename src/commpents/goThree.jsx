f32c8f172268a7978a6d1e086c546289704e5477
yumuysl <yinchuanyjw@outlook.com>
2025-05-15 13:33:33 +0800

    更新three

--- src/commpents/goThree.jsx
+++ src/commpents/goThree.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+
 function GoThree(){
+  const [count, setCount] = useState(0);
+
+
+  function changeCount(e){
+    console.log("点击我：", e) 
+    setCount(count + 1);
+  }
+
   return (
     <>
       <div>
         这是three页面
+        <div onClick={(e)=>changeCount(e)}>点击我{count}</div>
       </div>
     </>
   );
